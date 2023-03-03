e108205203ab4de77ed96de9e7ba0dc3d1bba003
igor-lourenco <igor.lourencosantos@gmail.com>
2023-03-03 19:02:07 -0300

    Criando o metodo findById

--- api/routes/pessoaRoute.js
+++ api/routes/pessoaRoute.js
@@ -4,5 +4,6 @@ const PessoaController = require('../../controllers/PessoaController.js');
 const router = Router();
 
 router.get('/pessoas', PessoaController.findAll);
+router.get('/pessoas/:id', PessoaController.findById);
 
 module.exports  = router;
--- controllers/PessoaController.js
+++ controllers/PessoaController.js
@@ -1,16 +1,34 @@
-const database = require('../api/models');
-
+const database = require("../api/models");
 
 class PessoaController {
-    static async findAll(req, res){
-        try{
-
-            const todasAsPessoas = await database.pessoas.findAll();
-            return res.status(200).json(todasAsPessoas);
-        }catch(erro){
-            return res.status(500).json(erro.message);
-        }
+  static async findAll(req, res) {
+    try {
+      const todasAsPessoas = await database.pessoas.findAll();
+      return res.status(200).json(todasAsPessoas);
+    } catch (erro) {
+      return res.status(500).json(erro.message);
     }
+  }
+
+  static async findById(req, res) {
+    const { id } = req.params;
+
+    try {
+      const p = await database.pessoas.findOne({
+        where: {
+          id: Number(id),
+        },
+      });
+
+      if (p !== null) {
+          return res.status(200).json(p);
+        } else {
+          return res.status(404).json({mensagem : `Id ${id} não encontrado!`});
+      }
+    } catch (erro) {
+      return res.status(500).json(erro.message);
+    }
+  }
 }
 
 module.exports = PessoaController;
