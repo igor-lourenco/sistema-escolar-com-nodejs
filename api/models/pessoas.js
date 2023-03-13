29859bdd004fc2be3ec518252c43fa24a66e1164
igor-lourenco <igor.lourencosantos@gmail.com>
2023-03-13 19:24:33 -0300

    Adicionando escopo de modelo

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -1,22 +1,33 @@
-'use strict';
+"use strict";
 
 /*
   Comando pra fazer a associação entre as tabelas:
   npx sequelize-cli db:migrate
 */
 module.exports = (sequelize, DataTypes) => {
-  const pessoas = sequelize.define('pessoas', {
-    nome: DataTypes.STRING,
-    ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
-    role: DataTypes.STRING
-  }, {paranoid: true});
-  pessoas.associate = function(models) {
+  const pessoas = sequelize.define(
+    "pessoas",
+    {
+      nome: DataTypes.STRING,
+      ativo: DataTypes.BOOLEAN,
+      email: DataTypes.STRING,
+      role: DataTypes.STRING,
+    },
+    {
+      paranoid: true,
+      defaultScope: {
+        where: {
+          ativo: true,
+        },
+      },
+    }
+  );
+  pessoas.associate = function (models) {
     pessoas.hasMany(models.Turmas, {
-      foreignKey: 'docente_id'
+      foreignKey: "docente_id",
     });
     pessoas.hasMany(models.Matriculas, {
-      foreignKey: 'estudante_id'
+      foreignKey: "estudante_id",
     });
   };
   return pessoas;
