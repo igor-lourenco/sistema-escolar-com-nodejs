bd6f2fa76a02a83630de5bd52a084694d83cd288
igor-lourenco <igor.lourencosantos@gmail.com>
2023-03-17 18:39:59 -0300

    API findAll Turmas filtrando por data_inicio

--- api/routes/TurmaRoute.js
+++ api/routes/TurmaRoute.js
@@ -3,7 +3,7 @@ const TurmaController = require('../../controllers/TurmaController.js');
 
 const router = Router();
 
-router.get('/turmas', TurmaController.findAll);
+router.get('/turmas', TurmaController.findAllFilterByData);
 router.get('/turmas/:id', TurmaController.findById);
 router.post('/turmas', TurmaController.insert);
 router.put('/turmas/:id', TurmaController.update);
--- controllers/TurmaController.js
+++ controllers/TurmaController.js
@@ -1,9 +1,28 @@
 const database = require("../api/models");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 class TurmaController {
   static async findAll(req, res) {
     try {
       const todasAsTurmas = await database.Turmas.findAll();
+      return res.status(200).json(todasAsTurmas);
+    } catch (erro) {
+      return res.status(500).json(erro.message);
+    }
+  }
+
+  static async findAllFilterByData(req, res) {
+    const {data_inicial, data_final} = req.query;
+    const where = {};
+
+    data_inicial || data_final ? where.data_inicio = {} : null //verific se esses parametros vinheram na requisição
+    data_inicial ? where.data_inicio[Op.gte] = data_inicial : null; // seta o valor da data_inicial dentro do objeto data_inicio
+    data_final ? where.data_inicio[Op.lte] = data_final : null;  // seta o valor da data_final dentro do objeto data_inicio
+
+    try {
+      const todasAsTurmas = await database.Turmas.findAll({where});
+      console.log(todasAsTurmas);
       return res.status(200).json(todasAsTurmas);
     } catch (erro) {
       return res.status(500).json(erro.message);
